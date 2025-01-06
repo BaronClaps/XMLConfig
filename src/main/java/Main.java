d986d62c342fd6bb0a631913c47e49b8cb8c52ce
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2025-01-06 13:27:25 -0600

    Create ways to make hubs with servos, motors, analog devices, i2c devices, and digital devices. then you can add ethernet devices and webcams and then create a robot. then you can export the xml.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,15 @@ import hub.components.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a new hub.components.Servo object
-        Servo servo = new Servo(Servo.Type.Servo, "Servo1", 0);
-        Analog a = new Analog(Analog.Type.AnalogInput, "Analog1", 0);
-        Digital d = new Digital(Digital.Type.DigitalDevice, "Digital1", 0);
-        Motor m = new Motor(Motor.Type.NeveRest20Gearmotor, "Motor1", 0);
-        I2C i = new I2C(I2C.Type.AdafruitBNO055IMU, "I2C1", 0, 1);
-        Digital d2 = new Digital(Digital.Type.Led, "Digital2", 1);
 
-        Hub hub = new HubBuilder(true)
-            .addServo(servo)
-            .addAnalog(a)
-            .addDigital(d)
-            .addMotor(m)
-            .addI2C(i)
-            .addDigital(d2)
-            .build();
+        Hub chub = new HubBuilder(true)
+                .addMotor(new Motor(Motor.Type.goBILDA5202SeriesMotor, "e", 1))
+                .addServo(new Servo(Servo.Type.Servo, "ea", 3))
+                .build();
 
         Robot robot = new RobotBuilder()
-                .addHub(hub)
+                .addHub(chub)
+                .addWebcam(new Webcam("weeeeeeee", "weeeeeeeeeeeeeeeee"))
                 .build();
 
         robot.save("robot.xml");
